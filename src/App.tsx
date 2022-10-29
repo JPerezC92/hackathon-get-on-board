4bc7489114d75b95ddf8b9e0392b11d1fbd01271
jbuendia1y <jgamer669@gmail.com>
2022-10-29 13:10:30 -0500

    Implement routes component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Button } from '@chakra-ui/react';
 import Layout from './layout';
+import { Routes } from './routes';
 
 function App() {
 	return (
 		<Layout>
 			<Button>HackaJob</Button>
+			<Routes />
 		</Layout>
 	);
 }
 
 export default App;
+
