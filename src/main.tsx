1e4d93dce7edfbf800a7c9a93e882d70e0a3e257
jbuendia1y <jgamer669@gmail.com>
2022-10-31 07:36:04 -0500

    removiendo import index.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,21 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { ChakraProvider } from '@chakra-ui/react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from "./App";
-import "./index.css";
-import { theme } from "./theme";
-import { QueryClient, QueryClientProvider } from "react-query";
+import App from './App';
+import theme from './theme';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 const queryClient = new QueryClient();
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider theme={theme}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ChakraProvider>
-    </QueryClientProvider>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<ChakraProvider theme={theme}>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</ChakraProvider>
+		</QueryClientProvider>
+	</React.StrictMode>,
 );
 
