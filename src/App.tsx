0624276dce3c674ad4981df918f5c7f04a6b2491
Joaquín Buendía <71197875+jbuendia1y@users.noreply.github.com>
2022-10-30 22:19:43 -0500

    Moviendo el BrowserRouter
    El BrowserRouter lo estoy moviendo al main.tsx como proveedor

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Nabvar } from './components/Navbar';
 import { HomePage } from './pages/HomePage';
 import { LoginPage } from './pages/LoginPage';
@@ -7,14 +7,11 @@ function App() {
 
 	return (
 		<>
-			<Router>
-				<Nabvar />
-
-				<Routes>
-					<Route path="/" element={<HomePage />} />
-					<Route path="/login" element={<LoginPage />} />
-				</Routes>
-			</Router>
+			<Nabvar />
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/login" element={<LoginPage />} />
+			</Routes>
 		</>
 	);
 }
