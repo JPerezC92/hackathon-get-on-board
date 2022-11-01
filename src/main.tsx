23bfaf799164910e8b8d9dfbe40986bbda6b6860
DomeScoppe <89617706+DomeScoppe@users.noreply.github.com>
2022-10-31 23:11:38 -0400

    remove index.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
 import { theme } from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
