da3d4a138c6251d7a224b6f4267760018ea181c2
DomeScoppe <89617706+DomeScoppe@users.noreply.github.com>
2022-11-03 02:12:50 -0400

    Fix db methods. Add jobApply & jobApllied components. Add routes for new components.

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -10,6 +10,8 @@ import Recover from '../pages/recover/Recover';
 import { RegisterPage } from '../pages/RegisterPage';
 import { webRoutes } from '../utilities/web.routes';
 import { OnlyGuestRoute, ProtectedRoute } from './components';
+import JobApply from '../pages/jobApply/JobApply';
+import JobsApplied from '../pages/jobsApplied/JobsApplied';
 
 const SignIn = lazy(() => import('../pages/signIn/SignIn'));
 const SignUp = lazy(() => import('../pages/signUp/SignUp'));
@@ -101,6 +103,23 @@ export function Router() {
 					element={
 						<ProtectedRoute>
 							<MyAccount />
+						</ProtectedRoute>
+					}
+				/>
+
+				<Route
+					path={webRoutes.apply}
+					element={
+						<ProtectedRoute>
+							<JobApply />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path={webRoutes.jobsApplied}
+					element={
+						<ProtectedRoute>
+							<JobsApplied />
 						</ProtectedRoute>
 					}
 				/>
