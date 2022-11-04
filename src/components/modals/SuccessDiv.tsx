d77e9574dfcf059ca38d47eeedf6d4b8b33d1346
DomeScoppe <89617706+DomeScoppe@users.noreply.github.com>
2022-11-04 04:27:09 -0400

    jobApply: add success modal after apply with redirect

--- src/components/modals/SuccessDiv.tsx
+++ src/components/modals/SuccessDiv.tsx
@@ -1,16 +1,29 @@
 import './Modals.css';
 import { useNavigate } from 'react-router-dom';
 import React from 'react';
+import { LSKeys } from '@/utilities/localStorageKeys';
+import { Job } from '@/models/job.model';
 
-const SuccessDiv = ({ children }: { children: React.ReactNode }) => {
+const SuccessDiv = ({ children, applying }: { children: React.ReactNode, applying:boolean }) => {
+
+	const clickHandler = () => {
+	if(applying){
+	const {id} = JSON.parse(window.localStorage.getItem(LSKeys.jobDetail) as string) as Job;
+		navigate(`/jobsV2/${id}`);
+		return false;}
+		navigate('/');
+
+	};
+
+
+
 	const navigate = useNavigate();
-	return (
-		<div className="successDivContainer" onClick={() => navigate('/')}>
-			<div className="successDiv">
+	 return (
+		<div className="successDivContainer" onClick={clickHandler}>
+			<div className={`successDiv ${applying ? 'transp' : ''}`}>
 				<p>{children}</p>
 			</div>
 		</div>
-	);
-};
+	);};
 
 export default SuccessDiv;
